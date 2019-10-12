c047959dc7f8fe4bb5894a006e774a7cd33aa0a3
Filip Sheronau <Filsheronov@gmail.com>
2019-10-12 22:18:26 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.minValue = null;
+        this.maxValue = null;
+        this.guessValue = null;
+    }
+        
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.guessValue = Math.round(this.minValue+((this.maxValue-this.minValue)/2));
+        return this.guessValue;
     }
 
     lower() {
-
+        this.maxValue = this.guessValue;
     }
 
     greater() {
-
+        this.minValue = this.guessValue;
     }
 }
 
